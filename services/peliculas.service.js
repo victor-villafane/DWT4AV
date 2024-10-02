20b5e13f337ad63cc5750af5fdeef1320af2e63e
victor-villafane <victor.villafane@davinci.edu.ar>
2024-10-02 16:52:41 -0300

    Actualizacion a mongo db atlas

--- services/peliculas.service.js
+++ services/peliculas.service.js
@@ -1,7 +1,7 @@
 import { MongoClient, ObjectId } from "mongodb"
 
-const client = new MongoClient("mongodb://localhost:27017")
-const db = client.db("PeliculasDB")
+const client = new MongoClient("mongodb+srv://admin:admin@dwt4av-hibridas-cluster.ekoej.mongodb.net/")
+const db = client.db("DWT4AV")
 
 async function getPeliculas(filtros = {}){
     const filterMongo = { eliminado: { $ne: true } }
@@ -15,39 +15,39 @@ async function getPeliculas(filtros = {}){
         filterMongo.$text = { $search: filtros.descripcion }
     }
     await client.connect()
-    return db.collection("peliculas").find( filterMongo ).toArray()
+    return db.collection("Peliculas").find( filterMongo ).toArray()
 }
 
 async function getPeliculaId(id_ingresado){
     await client.connect()
     console.log("id recibido", id_ingresado)
-    const datos = await db.collection("peliculas").findOne( { _id: ObjectId.createFromHexString(id_ingresado) } )  
+    const datos = await db.collection("Peliculas").findOne( { _id: ObjectId.createFromHexString(id_ingresado) } )  
     return datos 
 }
 
 async function agregarPelicula(pelicula){
     console.log(pelicula)
     await client.connect()
-    await db.collection("peliculas").insertOne(pelicula)
+    await db.collection("Peliculas").insertOne(pelicula)
     return pelicula
 }
 
 async function eliminarPelicula(id_ingresado){
     await client.connect()
-    await db.collection("peliculas").updateOne({ _id: ObjectId.createFromHexString(id_ingresado)}, { $set: {
+    await db.collection("Peliculas").updateOne({ _id: ObjectId.createFromHexString(id_ingresado)}, { $set: {
         eliminado: true
     } })
     return id_ingresado
 }
 const modificarPelicula = async (id_ingresado, peliculaActualizada) => {
     await client.connect()
-    await db.collection("peliculas").replaceOne({ _id: ObjectId.createFromHexString(id_ingresado)}, peliculaActualizada)
+    await db.collection("Peliculas").replaceOne({ _id: ObjectId.createFromHexString(id_ingresado)}, peliculaActualizada)
     return peliculaActualizada
 }
 
 const actualizarPelicula = async (id, peliculaActualizada) => {
     await client.connect()
-    const peliculaNueva = await db.collection("peliculas").updateOne({_id: ObjectId.createFromHexString(id)}, { $set: peliculaActualizada })
+    const peliculaNueva = await db.collection("Peliculas").updateOne({_id: ObjectId.createFromHexString(id)}, { $set: peliculaActualizada })
     return peliculaNueva
 }
 
